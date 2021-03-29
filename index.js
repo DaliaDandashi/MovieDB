de42b5af47c6b55d659d6296b78c0ee576e93c2c
DaliaDandashi <78696190+DaliaDandashi@users.noreply.github.com>
2021-03-29 16:13:25 +0300

    step3

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.get('/', (req, res) => {
+    res.send('ok')
+  })
+  app.get('/test', (req, res) => {
+    res.send(
+        {
+             status:200,
+             message:"ok"
+        }
+    )
+  })
+  app.get('/time', (req, res) => {
+  const today = new Date();
+  const time = today.getHours() + ":" + today.getSeconds();
+    res.send(
+      {status:200, message:time}
+    )
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  }) 
+  
